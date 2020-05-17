e44ec0eddf454e05be906c0bd74409d43bb80cce
Gajendra Pandeya <gajendrapandeya6@gmail.com>
2020-05-17 13:42:08 +0545

    Cart System Completed

--- app/src/main/java/com/codermonkeys/sampleapp/MainActivity.java
+++ app/src/main/java/com/codermonkeys/sampleapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.Menu;
 import android.widget.FrameLayout;
 
 import com.codermonkeys.sampleapp.fragments.HomeFragment;
+import com.codermonkeys.sampleapp.fragments.MyCartFragment;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
@@ -28,8 +29,15 @@ import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+    //Widget component's
     private DrawerLayout drawer;
     private FrameLayout frameLayout;
+
+    //var's
+   private NavigationView navigationView;
+   public static final int HOME_FRAGMENT = 0;
+   public static final int CART_FRAGMENT = 1;
+   private static int currentFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +50,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         frameLayout = findViewById(R.id.main_frame_layout);
 
         drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         navigationView.getMenu().getItem(0).setChecked(true);
 
@@ -51,14 +59,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         toggle.syncState();
 
 
-        setFragment(new HomeFragment());
+        setFragment(new HomeFragment(), HOME_FRAGMENT);
 
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        if(currentFragment == HOME_FRAGMENT) {
+            getMenuInflater().inflate(R.menu.main, menu);
+        }
         return true;
     }
 
@@ -71,6 +81,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
             case R.id.main_cart_icon:
                 //todo: notification system
+                myCart();
                 break;
 
             case R.id.main_notification_icon:
@@ -84,6 +95,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.nav_my_mall:
+                setFragment(new HomeFragment(), HOME_FRAGMENT);
                 break;
 
             case R.id.nav_my_orders:
@@ -93,6 +105,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 break;
 
             case R.id.nav_my_cart:
+                myCart();
                 break;
 
             case R.id.nav_my_wishlist:
@@ -108,8 +121,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         return true;
     }
 
-    private void setFragment(Fragment fragment) {
+    private void myCart() {
+        invalidateOptionsMenu();
+        setFragment(new MyCartFragment(), CART_FRAGMENT);
+        navigationView.getMenu().getItem(0).setChecked(true);
+    }
+    private void setFragment(Fragment fragment, int fragmentNo) {
 
+        currentFragment = fragmentNo;
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(frameLayout.getId(), fragment);
         fragmentTransaction.commit();
---
+++ app/src/main/java/com/codermonkeys/sampleapp/models/CartItemModel.java
@@ -0,0 +1,164 @@
+package com.codermonkeys.sampleapp.models;
+
+public class CartItemModel {
+
+    public static final int CART_ITEM = 0;
+    public static final int TOTAL_AMOUNT = 1;
+
+    private int type;
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    ////Cart item
+    private int productImage;
+    private int freeCoupens;
+    private int productQuantity;
+    private String productTitle;
+    private String productPrice;
+    private String cuttedPrice;
+    private int offersApplied;
+    private int coupensApplied;
+
+    public CartItemModel(int type, int productImage, int freeCoupens, int productQuantity, String productTitle, String productPrice, String cuttedPrice, int offersApplied, int coupensApplied) {
+        this.type = type;
+        this.productImage = productImage;
+        this.freeCoupens = freeCoupens;
+        this.productQuantity = productQuantity;
+        this.productTitle = productTitle;
+        this.productPrice = productPrice;
+        this.cuttedPrice = cuttedPrice;
+        this.offersApplied = offersApplied;
+        this.coupensApplied = coupensApplied;
+    }
+
+    public int getProductImage() {
+        return productImage;
+    }
+
+    public void setProductImage(int productImage) {
+        this.productImage = productImage;
+    }
+
+    public int getFreeCoupens() {
+        return freeCoupens;
+    }
+
+    public void setFreeCoupens(int freeCoupens) {
+        this.freeCoupens = freeCoupens;
+    }
+
+    public int getProductQuantity() {
+        return productQuantity;
+    }
+
+    public void setProductQuantity(int productQuantity) {
+        this.productQuantity = productQuantity;
+    }
+
+    public String getProductTitle() {
+        return productTitle;
+    }
+
+    public void setProductTitle(String productTitle) {
+        this.productTitle = productTitle;
+    }
+
+    public String getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(String productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public String getCuttedPrice() {
+        return cuttedPrice;
+    }
+
+    public void setCuttedPrice(String cuttedPrice) {
+        this.cuttedPrice = cuttedPrice;
+    }
+
+    public int getOffersApplied() {
+        return offersApplied;
+    }
+
+    public void setOffersApplied(int offersApplied) {
+        this.offersApplied = offersApplied;
+    }
+
+    public int getCoupensApplied() {
+        return coupensApplied;
+    }
+
+    public void setCoupensApplied(int coupensApplied) {
+        this.coupensApplied = coupensApplied;
+    }
+
+    ///Cart item
+
+
+    ////cart total
+    private String  totalItems;
+    private String totalItemPrice;
+    private String  deliveryPrice;
+    private String savedAmount;
+    private String totalAmount;
+
+    public CartItemModel(int type, String totalItems, String totalItemPrice, String deliveryPrice, String totalAmount, String savedAmount) {
+        this.type = type;
+        this.totalItems = totalItems;
+        this.totalItemPrice = totalItemPrice;
+        this.deliveryPrice = deliveryPrice;
+        this.savedAmount = savedAmount;
+        this.totalAmount = totalAmount;
+    }
+
+    public String getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(String totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public String  getTotalItems() {
+        return totalItems;
+    }
+
+    public void setTotalItems(String  totalItems) {
+        this.totalItems = totalItems;
+    }
+
+    public String getTotalItemPrice() {
+        return totalItemPrice;
+    }
+
+    public void setTotalItemPrice(String totalItemPrice) {
+        this.totalItemPrice = totalItemPrice;
+    }
+
+    public String getDeliveryPrice() {
+        return deliveryPrice;
+    }
+
+    public void setDeliveryPrice(String deliveryPrice) {
+        this.deliveryPrice = deliveryPrice;
+    }
+
+    public String getSavedAmount() {
+        return savedAmount;
+    }
+
+    public void setSavedAmount(String savedAmount) {
+        this.savedAmount = savedAmount;
+    }
+
+    ////cart total
+}
